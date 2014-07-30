4f09860fe11cd61cfda6ce75a14eedd19fdeb928
unknown <jaimewo@gmail.com>
2014-07-29 22:59:28 -0300

    Enviando mais controllers

---
+++ src/java/controller/createLocalInformarValores.java
@@ -0,0 +1,106 @@
+package controller;
+
+import dao.LocalDao;
+import dao.LocalQuantidadeDao;
+import dao.MunicipioLocalDao;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import jxl.read.biff.BiffException;
+import model.Arvore;
+import model.CoordenadaLocal;
+import model.EstatisticaInventario;
+import model.Local;
+import model.LocalQuantidade;
+import model.Municipio;
+import model.MunicipioLocal;
+import model.Parcela;
+/**
+ *
+ * @author jaime
+ */
+public class createLocalInformarValores extends HttpServlet {
+
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, Exception {
+
+        
+    //Receber da da tela
+    int idLocal = 0; //Pegar idLocal da tela
+    double qtdeBiomassa = 0; //Receber da tela
+    double qtdeCarbono = 0; //Receber da tela
+    double qtdeVolume = 0; //Receber da tela
+    
+    LocalQuantidade localQuantidade = new LocalQuantidade();
+    localQuantidade.setIdLocal(idLocal);
+    localQuantidade.setIdVariavelInteresse(1);  //Biomassa
+    localQuantidade.setIdMetodoCalculo(1); //Equacao
+    localQuantidade.setQtde(qtdeBiomassa);
+    
+    LocalQuantidadeDao localQuantidadeDao = new LocalQuantidadeDao();
+    localQuantidadeDao.cadastrar(localQuantidade);
+
+    
+    localQuantidade = new LocalQuantidade();
+    localQuantidade.setIdLocal(idLocal);
+    localQuantidade.setIdVariavelInteresse(2);  //Carbono
+    localQuantidade.setIdMetodoCalculo(1); //Equacao
+    localQuantidade.setQtde(qtdeCarbono);
+    
+    localQuantidadeDao = new LocalQuantidadeDao();
+    localQuantidadeDao.cadastrar(localQuantidade);
+
+    
+    localQuantidade = new LocalQuantidade();
+    localQuantidade.setIdLocal(idLocal);
+    localQuantidade.setIdVariavelInteresse(3);  //Volume
+    localQuantidade.setIdMetodoCalculo(1); //Equacao
+    localQuantidade.setQtde(qtdeVolume);
+    
+    localQuantidadeDao = new LocalQuantidadeDao();
+    localQuantidadeDao.cadastrar(localQuantidade);
+    
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(createLocalInformarValores.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(createLocalInformarValores.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(createLocalInformarValores.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(createLocalInformarValores.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+  
+    @Override
+    public String getServletInfo() {
+        return "Criar Local";
+    }// </editor-fold>
+    
+}
